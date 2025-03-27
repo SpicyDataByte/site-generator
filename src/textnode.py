cda17e77db96eaffcb62373e2138297699219dda
SpicyDataByte <matt@hildebrandt.club>
2025-03-27 11:47:48 -0600

    update core files

--- src/textnode.py
+++ src/textnode.py
@@ -9,12 +9,11 @@ class TextType(Enum):
     LINK = "link"
     IMAGE = "image"
 
-class TextNode():
+class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
-        self.url = url 
-    
+        self.url = url
     def __eq__(self, other):
         return (
             self.text_type == other.text_type
@@ -24,6 +23,7 @@ class TextNode():
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
 def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.TEXT:
         return LeafNode(None, text_node.text)
